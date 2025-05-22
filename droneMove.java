6aa7cc0de10a521e30810aaa68f9f8c5fcd88c6b
Me87 <63875415+Me871@users.noreply.github.com>
2025-05-22 08:58:22 -0400

    Update droneMove.java: Added Gunner controls and optimized code

--- droneMove.java
+++ droneMove.java
@@ -1,3 +1,5 @@
+// This program is designed to be testable in https://ftcsim.org
+
 public class MyFIRSTJavaOpMode extends LinearOpMode {
     DcMotor backLeftDrive;
     DcMotor backRightDrive;
@@ -13,62 +15,42 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
 var fwd, left, cw;
     
     
-    @Override
-    public void runOpMode() {
-      Gamepad gamepad1 = new FTCGamepad();
-      backLeftDrive = hardwareMap.get(DcMotor.class, "backLeftDrive");
-      backRightDrive = hardwareMap.get(DcMotor.class, "backRightDrive");
-      frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeftDrive");
-      frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
-      armTilt = hardwareMap.get(DcMotor.class, "armTilt");
-      armExtend = hardwareMap.get(DcMotor.class, "armExtend");
-      claw = hardwareMap.get(DcMotor.class, "claw");
-      color1 = hardwareMap.get(ColorSensor.class, "color1");
-      distance1 = hardwareMap.get(DistanceSensor.class, "distance1");
-      imu = hardwareMap.get(BNO055IMU.class, "imu");
-      frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
-      backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
-      while (opModeIsActive()) {
-        fwd = gamepad1.right_stick_y
-        left = gamepad1.right_stick_x
-        cw = gamepad1.left_stick_x
-        if (gamepad1.right_trigger) {
-          fwd = fwd * 0.5;
-          left = left * 0.5;
-          cw = cw * 0.5;
-        }
-        frontLeftDrive.setPower(fwd + -1 * left + -1 * cw);
-        frontRightDrive.setPower(fwd + left + cw);
-        backLeftDrive.setPower(fwd + left + -1 * cw);
-        backRightDrive.setPower(fwd + -1 * left + cw);
-        if (gamepad1.pressKey(103)) {
-          armTilt.setPower(0.5);
-        } else {
-          if (gamepad1.pressKey(116)) {
-            armTilt.setPower(-0.5);
-          } else {
-            armTilt.setPower(0);
-          }
-        }
-        if (gamepad1.pressKey(102)) {
-          armExtend.setPower(0.5);
-        } else {
-          if (gamepad1.pressKey(118)) {
-            armExtend.setPower(-0.5);
-          } else {
-            armExtend.setPower(0);
-          }
-        }
-        if (gamepad1.pressKey(104)) {
-          claw.setPower(1);
-        } else {
-          if (gamepad1.pressKey(110)) {
-            claw.setPower(-1);
-          } else {
-            claw.setPower(0);
-          }
-        }
-      }
-    }
+@Override // Clears any configurations from a previous run.
+
+public void runOpMode() {
+  // Define hardware names here.
+  Gamepad gamepad1 = new FTCGamepad();
+  Gamepad gamepad2 = new FTCGamepad();
+  backLeftDrive = hardwareMap.get(DcMotor.class, "backLeftDrive");
+  backRightDrive = hardwareMap.get(DcMotor.class, "backRightDrive");
+  frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeftDrive");
+  frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
+  imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+  // Adjust hardware settings here
+  frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+  backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+      
+while (opModeIsActive()) {
+  // Get the movement (translation and rotation) inputs. Since the Logitech F310 inverts the Y axis, we must invert again here. The Pilot can block the Gunner's movement controls with either bumper.
+  if (gamepad1.left_bumper || gamepad1.right_bumper) {
+    fwd = -gamepad1.right_stick_y;
+    left = gamepad1.right_stick_x;
+    cw = gamepad1.left_stick_x;
+  else
+    fwd = -gamepad1.right_stick_y + (-gamepad2.right_stick_y * 0.25); // We slow down the Gunner's controls so that they can make finer movements.
+    left = gamepad1.right_stick_x + (gamepad2.right_stick_x * 0.25);
+    cw = gamepad1.left_stick_x;
+  }
     
+  // Make it so that either trigger can slow down the robot.
+  fwd = fwd * (1 - gamepad1.left_trigger - gamepad1.right_trigger);
+  left = left * (1 - gamepad1.left_trigger - gamepad1.right_trigger);
+  cw = cw * (1 - gamepad1.left_trigger - gamepad1.right_trigger);
+
+  // Use the fwd, left, and cw variables to move the robot. (This assumes that we use the Gobuilda Omniwheels so we see an "X" formed from the top)
+  frontLeftDrive.setPower(fwd + -left + cw);
+  frontRightDrive.setPower(fwd + left + -cw);
+  backLeftDrive.setPower(fwd + left + cw);
+  backRightDrive.setPower(fwd + -left + -cw);  
 }
